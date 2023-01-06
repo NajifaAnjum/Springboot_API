1acfd12e2b19dd4cf318e7f410aea56cce363b7b
najifaanjum <najifaanjum2015@gmail.com>
2023-01-07 00:36:44 +0600

    POST_method_for_inserting_new_topic_element

---
+++ 5.POST_method_for_inserting_new_topic_element/src/main/java/com/example/demo/topic/TopicService.java
@@ -0,0 +1,33 @@
+package com.example.demo.topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
+            new Topic("spring", "Spring framework", "Spring framework description"),
+            new Topic("java", "core java framework", "java framework description"),
+            new Topic("javascript", "javascript framework", "javascript framework description")
+    ));
+
+    //method that returns the list of topics
+    public List<Topic> getAllTopics(){
+        return topics;
+    }
+
+    //method that returns(gets/shows) a single topic
+    public Topic getTopic(String id){
+        return topics.stream().filter(topic -> topic.getId().equals(id)).findFirst().get();           //comparing the id of topic to the id that's passed in. findFirst() to get the first topic element.
+    }
+
+    //POST 
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+}
+
+
